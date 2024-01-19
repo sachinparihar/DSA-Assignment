de0dccc2960abcf79496a037445a48cf701cdf58
sachinparihar <sachinparihar98120@gmail.com>
2024-01-19 12:02:42 +0530

    solve question

---
+++ Java/Sorting/sortColor.java
@@ -0,0 +1,36 @@
+package Sorting;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void sortColors(int[] nums) {
+        int low = 0, mid = 0, high = nums.length - 1;
+
+        while (mid <= high) {
+            if (nums[mid] == 0) {
+                int temp = nums[low];
+                nums[low] = nums[mid];
+                nums[mid] = temp;
+                low++;
+                mid++;
+            } else if (nums[mid] == 1) {
+                mid++;
+            } else if (nums[mid] == 2) {
+                int temp = nums[mid];
+                nums[mid] = nums[high];
+                nums[high] = temp;
+                high--;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {2, 0, 2, 1, 1, 0};
+        sortColors(nums);
+        System.out.println(Arrays.toString(nums)); // Output: [0, 0, 1, 1, 2, 2]
+
+        nums = new int[]{2, 0, 1};
+        sortColors(nums);
+        System.out.println(Arrays.toString(nums)); // Output: [0, 1, 2]
+    }
+}
